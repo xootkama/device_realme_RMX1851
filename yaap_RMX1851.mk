5ba9be14d69b04dc60e1ae277f81d03d0c5e6306
X00T Best Kernels <88646493+xootkama@users.noreply.github.com>
2021-11-07 11:49:24 +0530

    Update and rename aosp_RMX1851.mk to yaap_RMX1851.mk

--- yaap_RMX1851.mk
+++ yaap_RMX1851.mk
@@ -18,19 +18,16 @@
 TARGET_GAPPS_ARCH=arm64
 TARGET_USES_FACE_UNLOCK := true
 
-# Hycon
-HYCON_MAINTAINER := TheTablaster
-
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common LOS stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/yaap/config/common_full_phone.mk)
 
 $(call inherit-product, device/realme/RMX1851/device.mk)
 
-PRODUCT_NAME := aosp_RMX1851
+PRODUCT_NAME := yaap_RMX1851
 PRODUCT_DEVICE := RMX1851
 PRODUCT_MANUFACTURER := Realme
 PRODUCT_BRAND := realme
