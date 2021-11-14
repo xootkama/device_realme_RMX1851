4f7edb97d47616dbac799457c6f46357b802c8f1
X00T Best Kernels <88646493+xootkama@users.noreply.github.com>
2021-11-14 06:58:26 +0530

    Update fuse_RMX1851.mk

--- fuse_RMX1851.mk
+++ fuse_RMX1851.mk
@@ -23,11 +23,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common LOS stuff.
-$(call inherit-product, vendor/yaap/config/common_full_phone.mk)
+$(call inherit-product, vendor/fuse/config/common_full_phone.mk)
 
 $(call inherit-product, device/realme/RMX1851/device.mk)
 
-PRODUCT_NAME := yaap_RMX1851
+PRODUCT_NAME := fuse_RMX1851
 PRODUCT_DEVICE := RMX1851
 PRODUCT_MANUFACTURER := Realme
 PRODUCT_BRAND := realme
