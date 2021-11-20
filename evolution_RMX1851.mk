8c50b94cb3c9d3b71ce1d3fd2104a9c534cbee6b
X00T Best Kernels <88646493+xootkama@users.noreply.github.com>
2021-11-20 19:06:41 +0530

    Update and rename aosp_RMX1851.mk to evolution_RMX1851.mk

--- evolution_RMX1851.mk
+++ evolution_RMX1851.mk
@@ -23,11 +23,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common LOS stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
 
 $(call inherit-product, device/realme/RMX1851/device.mk)
 
-PRODUCT_NAME := aosp_RMX1851
+PRODUCT_NAME := evolution_RMX1851
 PRODUCT_DEVICE := RMX1851
 PRODUCT_MANUFACTURER := Realme
 PRODUCT_BRAND := realme
